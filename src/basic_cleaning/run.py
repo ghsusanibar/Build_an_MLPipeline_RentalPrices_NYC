610179037caa1adc59d29fcef0d6c8e89ca29c51
ghsusanibar <ghsusanibar@pucp.pe>
2021-06-30 10:37:35 -0500

    adding docstring to basic_cleaning step

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -15,15 +15,10 @@ logger = logging.getLogger()
 def go(args):
 
     run = wandb.init(job_type="basic_cleaning")
-    #run.config.update(args)
+    """
+    This function downloads a raw dataset and apply some basic data cleaning. Then the function uploads the result to a new artifact on W&B
+    """
 
-    # Download input artifact. This will also log that this script is using this
-    # particular version of the artifact
-    # artifact_local_path = run.use_artifact(args.input_artifact).file()
-
-    ######################
-    # YOUR CODE HERE     #
-    ######################
     logger.info("Downloading artifact")
     artifact = run.use_artifact(args.input_artifact)
     artifact_path = artifact.file()
@@ -56,49 +51,85 @@ def go(args):
 
 
 if __name__ == "__main__":
+    """
+    This function downloads a raw dataset and apply some basic data cleaning. Then the function uploads the result to a new artifact on W&B.
+    
+    Parameters
+    ----------
+    input_artifact: str
+        Fully qualified name for the artifact
+    output_artifact: str
+        Name for the output artifact
+    output_type: str
+        Type for the output artifact
+    output_description: str
+        Description for the output artifact
+    min_price: float
+        Minimum price to be considered
+    max_price: float
+        Maximum price to be considered
+    
+    Returns
+    -------
+    This function does not return a value, it uploads the cleaned dataset to Weights & Biases.
+    
+    Examples
+    --------
+    >>> mlflow.run(
+                 <<file_path_basic_clieaning_directory>>,
+                 <<entrypoint>>,
+                 parameters={
+                     "input_artifact": "sample.csv:latest",
+                     "output_artifact": "clean_sample.csv",
+                     "output_type": "clean_sample",
+                     "output_description": "Data with outliers and null values removed",
+                     "min_price": 50,
+                     "max_price": 350
+                 },
+             )
+    """
 
     parser = argparse.ArgumentParser(description="A very basic data cleaning", fromfile_prefix_chars="@",)
 
-
     parser.add_argument(
         "--input_artifact", 
-        type=str,## INSERT TYPE HERE: str, float or int,
-        help="Fully-qualified name for the input artifact",## INSERT DESCRIPTION HERE,
+        type=str,
+        help="Fully-qualified name for the input artifact",
         required=True
     )
 
     parser.add_argument(
         "--output_artifact", 
-        type=str,## INSERT TYPE HERE: str, float or int,
-        help="Name for the artifact",## INSERT DESCRIPTION HERE,
+        type=str,
+        help="Name for the output artifact",
         required=True
     )
 
     parser.add_argument(
         "--output_type", 
-        type=str,## INSERT TYPE HERE: str, float or int,
-        help="Type for the artifact",## INSERT DESCRIPTION HERE,
+        type=str,
+        help="Type for the output artifact",
         required=True
     )
 
     parser.add_argument(
         "--output_description", 
-        type=str,## INSERT TYPE HERE: str, float or int,
-        help="Description for the artifact",## INSERT DESCRIPTION HERE,
+        type=str,
+        help="Description for the output artifact",
         required=True
     )
 
     parser.add_argument(
         "--min_price", 
-        type=float,## INSERT TYPE HERE: str, float or int,
-        help="Minimum price to be considered",## INSERT DESCRIPTION HERE,
+        type=float,
+        help="Minimum price to be considered",
         required=True
     )
 
     parser.add_argument(
         "--max_price", 
-        type=float,## INSERT TYPE HERE: str, float or int,
-        help="Maximum price to be considered",## INSERT DESCRIPTION HERE,
+        type=float,
+        help="Maximum price to be considered",
         required=True
     )
 
